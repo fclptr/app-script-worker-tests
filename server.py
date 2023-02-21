b8c45bad63fb58e5280fa36d5dfa70372ccd87c8
Piotr <fclptr@gmail.com>
2023-02-21 08:57:01 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,13 +4,17 @@ import socketserver
 
 from http import HTTPStatus
 
+src='''
+function version_of_lib() {
+  return "Wersja taka owaka...";
+}
+'''
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+        self.end_headers()        
+        self.wfile.write(src.encode())
 
 
 port = int(os.getenv('PORT', 80))
